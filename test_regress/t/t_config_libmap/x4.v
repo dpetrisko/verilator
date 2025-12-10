cb7cfec695519f060024b1eef9f6eabdf66434cc
Dan Ruelas-Petrisko <petrisko.bsg@gmail.com>
2025-12-09 16:32:35 -0800

    more coverage nits

--- test_regress/t/t_config_libmap/x4.v
+++ test_regress/t/t_config_libmap/x4.v
@@ -4,15 +4,6 @@
 // any use, without warranty, 2025 by Wilson Snyder.
 // SPDX-License-Identifier: CC0-1.0
 
-module t;
-  m1 u_1();
-  m2 u_2();
-  m3 u_3();
-  x4 u_4();
-  other u_o();
-
-  initial begin
-    $write("*-* All Finished *-*\n");
-    $finish;
-  end
+module x4;
+  initial $display("x4 %%m=%m %%l=%l");
 endmodule
